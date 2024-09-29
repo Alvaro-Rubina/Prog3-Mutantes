233d7a69ff5102e75d467c0be25d7484f9f35263
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-29 14:49:10 -0300

    Creada entidad Human

---
+++ src/main/java/org/alvarub/mutantes/model/entity/Human.java
@@ -0,0 +1,23 @@
+package org.alvarub.mutantes.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter @Setter
+public class Human {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ElementCollection
+    private List<String> dna;
+    private boolean isMutant = false;
+}
