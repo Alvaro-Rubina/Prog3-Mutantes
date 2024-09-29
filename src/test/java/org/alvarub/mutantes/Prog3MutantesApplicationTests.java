ad0dac046175aeeb5472e848c1a4d4c471df5867
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-29 12:41:12 -0300

    Primer commit
    configuraciones automáticas iniciales

---
+++ src/test/java/org/alvarub/mutantes/Prog3MutantesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alvarub.mutantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3MutantesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
