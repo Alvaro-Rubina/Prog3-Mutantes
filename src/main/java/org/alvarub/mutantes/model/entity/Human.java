d9f124556386599adaf7304d9b96e99136579c10
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-30 06:28:18 -0300

    Creada excepción DnaNotValidException

--- src/main/java/org/alvarub/mutantes/model/entity/Human.java
+++ src/main/java/org/alvarub/mutantes/model/entity/Human.java
@@ -3,7 +3,6 @@ package org.alvarub.mutantes.model.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
---
+++ src/main/java/org/alvarub/mutantes/utils/exceptions/DnaNotValidException.java
@@ -0,0 +1,7 @@
+package org.alvarub.mutantes.utils.exceptions;
+
+public class DnaNotValidException extends RuntimeException {
+    public DnaNotValidException(String message) {
+        super(message);
+    }
+}
