986e5ba0c3ac8a9aa574f2c150905ea6e9f26ba2
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-02 22:02:06 -0300

    Arreglado error en la declaracion de HumanRepository

--- src/main/java/org/alvarub/mutantes/repository/HumanRepository.java
+++ src/main/java/org/alvarub/mutantes/repository/HumanRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HumanRepository extends JpaRepository<Long, Human> {
+public interface HumanRepository extends JpaRepository<Human, Long> {
 }
