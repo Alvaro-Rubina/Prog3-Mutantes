7cb91d121fbadf12c61c3ecbe4dc686f578b2046
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-04 18:09:20 -0300

    Creado MutantDetectorService
    Se creó la clase MutantDetectorService, donde están los metodos encargados de verificar el adn mutante cargado.

--- src/main/java/org/alvarub/mutantes/service/HumanService.java
+++ src/main/java/org/alvarub/mutantes/service/HumanService.java
@@ -19,10 +19,13 @@ public class HumanService {
     @Autowired
     private HumanMapper humanMapper;
 
-    public boolean saveHuman(HumanDTO humanDTO) {
-        validateDna(humanDTO.dna());
+    @Autowired
+    private MutantDetectorService mutantDetectorService;
 
-        boolean isMutant = verifyMutant(humanDTO.dna());
+    public boolean saveHuman(HumanDTO humanDTO) {
+        mutantDetectorService.validateDna(humanDTO.dna());
+
+        boolean isMutant = mutantDetectorService.verifyMutant(humanDTO.dna());
 
         Human human = humanMapper.humanDTOToHuman(humanDTO);
         human.setMutant(isMutant);
@@ -32,113 +35,5 @@ public class HumanService {
         return isMutant;
     }
 
-    // Valida que el dna se componga de 6 strings con 6 caracteres c/u y solo contengan A, T, C y G
-    private void validateDna(List<String> dna) {
-        if (dna.size() != 6) {
-            throw new DnaNotValidException("The DNA must have exactly 6 strands.");
-        }
 
-        for (String str : dna) {
-            if (str.length() != 6) {
-                throw new DnaNotValidException("Each DNA strand must be exactly 6 characters long.");
-
-            } else if (!str.toUpperCase().matches("[ATCG]+")) {
-                throw new DnaNotValidException("The DNA must only contain the letters A, T, C and G.");
-            }
-        }
-    }
-
-    // Cuenta las secuencias horizontales, verticales y diagonales. Para cada caso, se comparan
-    // los 3 caracteres sucesivos sin salirse de los límites de la matriz.
-    private boolean verifyMutant(List<String> dna) {
-        int sequences = 0;
-        String[] dnaArray = dna.toArray(new String[0]);
-        int length = dnaArray.length;
-
-        // Horizontal
-        for (int fil = 0; fil < length; fil++) {
-            for (int col = 0; col < length; col++) {
-                if (col + 3 < length) {
-                    char currentChar = dnaArray[fil].charAt(col);
-                    if (dnaArray[fil].charAt(col) == dnaArray[fil].charAt(col + 1) &&
-                            dnaArray[fil].charAt(col) == dnaArray[fil].charAt(col + 2) &&
-                            dnaArray[fil].charAt(col) == dnaArray[fil].charAt(col + 3)) {
-                        sequences++;
-
-                        while (col + 1 < length && dnaArray[fil].charAt(col + 1) == currentChar) {
-                            col++;
-                        }
-
-                        if (sequences > 1) return true;
-                    }
-                }
-            }
-        }
-
-        // Vertical
-        for (int fil = 0; fil < length; fil++) {
-            for (int col = 0; col < length; col++) {
-                if (fil + 3 < length) {
-                    char currentChar = dnaArray[fil].charAt(col);
-                    if (dnaArray[fil].charAt(col) == dnaArray[fil + 1].charAt(col) &&
-                            dnaArray[fil].charAt(col) == dnaArray[fil + 2].charAt(col) &&
-                            dnaArray[fil].charAt(col) == dnaArray[fil + 3].charAt(col)) {
-                        sequences++;
-
-                        while (fil + 1 < length && dnaArray[fil + 1].charAt(col) == currentChar) {
-                            fil++;
-                        }
-
-                        if (sequences > 1) return true;
-                    }
-                }
-            }
-        }
-
-        // Diagonal
-        for (int fil = 0; fil < length; fil++) {
-            for (int col = 0; col < length; col++) {
-                if (fil + 3 < length && col + 3 < length) {
-                    char currentChar = dnaArray[fil].charAt(col);
-                    if (dnaArray[fil].charAt(col) == dnaArray[fil + 1].charAt(col + 1) &&
-                            dnaArray[fil].charAt(col) == dnaArray[fil + 2].charAt(col + 2) &&
-                            dnaArray[fil].charAt(col) == dnaArray[fil + 3].charAt(col + 3)) {
-                        sequences++;
-
-                        while (fil + 1 < length && col + 1 < length &&
-                                dnaArray[fil + 1].charAt(col + 1) == currentChar) {
-                            fil++;
-                            col++;
-                        }
-
-                        if (sequences > 1) return true;
-                    }
-                }
-            }
-        }
-
-        // Diagonal inversa
-        for (int fil = 0; fil < length; fil++) {
-            for (int col = 0; col < length; col++) {
-                if (fil + 3 < length && col - 3 >= 0) {
-                    char currentChar = dnaArray[fil].charAt(col);
-                    if (dnaArray[fil].charAt(col) == dnaArray[fil + 1].charAt(col - 1) &&
-                            dnaArray[fil].charAt(col) == dnaArray[fil + 2].charAt(col - 2) &&
-                            dnaArray[fil].charAt(col) == dnaArray[fil + 3].charAt(col - 3)) {
-                        sequences++;
-
-                        while (fil + 1 < length && col - 1 >= 0 &&
-                                dnaArray[fil + 1].charAt(col - 1) == currentChar) {
-                            fil++;
-                            col--;
-                        }
-                        
-                        if (sequences > 1) return true;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
 }
---
+++ src/main/java/org/alvarub/mutantes/service/MutantDetectorService.java
@@ -0,0 +1,133 @@
+package org.alvarub.mutantes.service;
+
+import org.alvarub.mutantes.utils.exceptions.DnaNotValidException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MutantDetectorService {
+
+    // Valida que el dna se componga de 6 strings con 6 caracteres c/u y solo contengan A, T, C y G
+    public void validateDna(List<String> dna) {
+        if (dna.size() != 6) {
+            throw new DnaNotValidException("The DNA must have exactly 6 strands.");
+        }
+
+        for (String str : dna) {
+            if (str.length() != 6) {
+                throw new DnaNotValidException("Each DNA strand must be exactly 6 characters long.");
+
+            } else if (!str.toUpperCase().matches("[ATCG]+")) {
+                throw new DnaNotValidException("The DNA must only contain the letters A, T, C and G.");
+            }
+        }
+    }
+
+    // Se cuentan las secuencias de 4 caracteres iguales en horizontal, vertical y diagonales.
+    // En cada caso, se procura no salirse de los límites de la matriz. Si se encuentran más de una secuencia, se retorna true.
+    public boolean verifyMutant(List<String> dna) {
+        int sequences = 0;
+        String[] dnaArray = dna.toArray(new String[0]);
+        int length = dnaArray.length;
+
+        // Horizontal
+        for (int fil = 0; fil < length; fil++) {
+            for (int col = 0; col < length - 3; col++) {
+                char currentChar = dnaArray[fil].charAt(col);
+
+                if (currentChar == dnaArray[fil].charAt(col + 1) &&
+                        currentChar == dnaArray[fil].charAt(col + 2) &&
+                        currentChar == dnaArray[fil].charAt(col + 3)) {
+
+                    sequences++;
+
+                    // Acá se procura no volver a contar la misma secuencia
+                    while (col + 1 < length && dnaArray[fil].charAt(col + 1) == currentChar) {
+                        col++;
+                    }
+
+                    if (sequences > 1) return true;
+                }
+            }
+        }
+
+        // Vertical
+        for (int col = 0; col < length; col++) {
+            for (int fil = 0; fil < length - 3; fil++) {
+                char currentChar = dnaArray[fil].charAt(col);
+
+                if (currentChar == dnaArray[fil + 1].charAt(col) &&
+                        currentChar == dnaArray[fil + 2].charAt(col) &&
+                        currentChar == dnaArray[fil + 3].charAt(col)) {
+
+                    sequences++;
+
+                    // Acá se procura no volver a contar la misma secuencia
+                    while (fil + 1 < length && dnaArray[fil + 1].charAt(col) == currentChar) {
+                        fil++;
+                    }
+
+                    System.out.println("Secuencias: " + sequences);
+                    if (sequences > 1) return true;
+                }
+            }
+        }
+
+        // Diagonal (de izquierda a derecha)
+        for (int fil = 0; fil < length - 3; fil++) {
+            for (int col = 0; col < length - 3; col++) {
+                char currentChar = dnaArray[fil].charAt(col);
+                if (currentChar == dnaArray[fil + 1].charAt(col + 1) &&
+                        currentChar == dnaArray[fil + 2].charAt(col + 2) &&
+                        currentChar == dnaArray[fil + 3].charAt(col + 3)) {
+
+                    sequences++;
+
+                    // Acá se procura no volver a contar la misma secuencia
+                    while (fil + 1 < length && col + 1 < length &&
+                            dnaArray[fil + 1].charAt(col + 1) == currentChar) {
+                        fil++;
+                        col++;
+                    }
+
+                    // Valido los límites para no salirme de la matriz
+                    fil = Math.min(fil, length - 4);
+                    col = Math.min(col, length - 4);
+
+                    System.out.println("Secuencias: " + sequences);
+                    if (sequences > 1) return true;
+                }
+            }
+        }
+
+        // Diagonal (de derecha a izquierda)
+        for (int fil = 0; fil < length - 3; fil++) {
+            for (int col = 3; col < length; col++) {
+                char currentChar = dnaArray[fil].charAt(col);
+                if (currentChar == dnaArray[fil + 1].charAt(col - 1) &&
+                        currentChar == dnaArray[fil + 2].charAt(col - 2) &&
+                        currentChar == dnaArray[fil + 3].charAt(col - 3)) {
+
+                    sequences++;
+                    
+                    // Acá se procura no volver a contar la misma secuencia
+                    while (fil + 1 < length && col - 1 >= 0 &&
+                            dnaArray[fil + 1].charAt(col - 1) == currentChar) {
+                        fil++;
+                        col--;
+                    }
+
+                    // Valido los límites para no salirme de la matriz
+                    fil = Math.min(fil, length - 4);
+                    col = Math.max(col, 3);
+
+                    System.out.println("Secuencias: " + sequences);
+                    if (sequences > 1) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
