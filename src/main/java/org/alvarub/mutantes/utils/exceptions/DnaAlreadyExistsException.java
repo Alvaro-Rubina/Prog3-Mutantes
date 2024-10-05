d8ae48b306da544547d71ccdb9553924a83c96b2
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-05 17:08:16 -0300

    Creado DnaAlreadyExistsException
    Se lanza cuando se inenta agregar un dna ya existente a la bbdd

---
+++ src/main/java/org/alvarub/mutantes/utils/exceptions/DnaAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package org.alvarub.mutantes.utils.exceptions;
+
+public class DnaAlreadyExistsException extends RuntimeException {
+    public DnaAlreadyExistsException(String message) {
+        super(message);
+    }
+}
