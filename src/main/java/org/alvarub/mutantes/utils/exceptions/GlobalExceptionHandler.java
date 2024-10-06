ff4f1eb86de20663d440b28f43e2d4f54ecd0392
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-06 16:08:55 -0300

    Actualizado ExceptionHandler
    Se añadió un caso para capturar excepciones de mal formato JSON

--- src/main/java/org/alvarub/mutantes/utils/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/alvarub/mutantes/utils/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package org.alvarub.mutantes.utils.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -33,6 +34,11 @@ public class GlobalExceptionHandler {
                 .map(error -> ((FieldError) error).getDefaultMessage())
                 .collect(Collectors.joining(". ")) + ".";
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
