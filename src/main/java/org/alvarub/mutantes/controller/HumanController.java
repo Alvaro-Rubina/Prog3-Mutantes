02edb340011a436bc55b8dbe4e9eb4f2e488eaaa
Alvaro Rubina <alvarorubina132@gmail.com>
2024-10-06 16:09:51 -0300

    Añadido endpoint /stats/

--- src/main/java/org/alvarub/mutantes/controller/HumanController.java
+++ src/main/java/org/alvarub/mutantes/controller/HumanController.java
@@ -2,13 +2,11 @@ package org.alvarub.mutantes.controller;
 
 import jakarta.validation.Valid;
 import org.alvarub.mutantes.model.dto.HumanDTO;
+import org.alvarub.mutantes.model.dto.StatsDTO;
 import org.alvarub.mutantes.service.IHumanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/human")
@@ -26,5 +24,11 @@ public class HumanController {
         } else {
             return ResponseEntity.status(403).body("Human detected.");
         }
+    }
+
+    @GetMapping("/stats/")
+    @ResponseBody
+    public ResponseEntity<StatsDTO> getStats(){
+        return ResponseEntity.ok(humanService.getStats());
     }
 }
